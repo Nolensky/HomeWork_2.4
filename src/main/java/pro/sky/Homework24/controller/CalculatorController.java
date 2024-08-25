0fe98aa13b80870f720aed8ab393f7e66dd16ccd
nolen <nolensky@mail.ru>
2024-08-26 01:11:45 +0300

    HW_2.4

--- src/main/java/pro/sky/Homework24/controller/CalculatorController.java
+++ src/main/java/pro/sky/Homework24/controller/CalculatorController.java
@@ -1,11 +1,13 @@
 package pro.sky.Homework24.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pro.sky.Homework24.service.CalculatorService;
 
-@RestController
+@RestController()
+@RequestMapping("/calculate")
 public class CalculatorController {
     private final CalculatorService calculatorService;
 
@@ -18,33 +20,28 @@ public class CalculatorController {
         return "Приветствуем вас в программе \"Калькулятор\"";
     }
 
-    @GetMapping("/calculate")
-    public String hello() {
-        return "Введите арифметическое действие и два числа";
-    }
-
-    @GetMapping("/calculate/plus")
+    @GetMapping("/plus")
     public String plus(
             @RequestParam(value = "num1", required = false) Integer number1,
             @RequestParam(value = "num2", required = false) Integer number2) {
         return calculatorService.plus(number1, number2);
     }
 
-    @GetMapping("/calculate/minus")
+    @GetMapping("/minus")
     public String minus(
             @RequestParam(value = "num1", required = false) Integer number1,
             @RequestParam(value = "num2", required = false) Integer number2) {
         return calculatorService.minus(number1, number2);
     }
 
-    @GetMapping("/calculate/multiply")
+    @GetMapping("/multiply")
     public String multiply(
             @RequestParam(value = "num1", required = false) Integer number1,
             @RequestParam(value = "num2", required = false) Integer number2) {
         return calculatorService.multiply(number1, number2);
     }
 
-    @GetMapping("/calculate/divide")
+    @GetMapping("/divide")
     public String divide(
             @RequestParam(value = "num1", required = false) Double number1,
             @RequestParam(value = "num2", required = false) Double number2) {
--- src/main/java/pro/sky/Homework24/service/CalculatorService.java
+++ src/main/java/pro/sky/Homework24/service/CalculatorService.java
@@ -8,32 +8,28 @@ public class CalculatorService {
     private Integer num1;
     private Integer num2;
 
-    public String plus(@RequestParam("num1") Integer number1,
-                       @RequestParam("num2") Integer number2) {
+    public String plus( Integer number1, Integer number2) {
         if (number1 == null || number2 == null) {
             return "Вы ввели неверные данные!!!";
         }
         return number1 + " + " + number2 + " = " + (number1 + number2);
     }
 
-    public String minus(@RequestParam("num1") Integer number1,
-                        @RequestParam("num2") Integer number2) {
+    public String minus(Integer number1, Integer number2) {
         if (number1 == null || number2 == null) {
             return "Вы ввели неверные данные!!!";
         }
         return number1 + " - " + number2 + " = " + (number1 - number2);
     }
 
-    public String multiply(@RequestParam("num1") Integer number1,
-                           @RequestParam("num2") Integer number2) {
+    public String multiply( Integer number1, Integer number2) {
         if (number1 == null || number2 == null) {
             return "Вы ввели неверные данные!!!";
         }
         return number1 + " * " + number2 + " = " + (number1 * number2);
     }
 
-    public String divide(@RequestParam("num1") Double number1,
-                         @RequestParam("num2") Double number2) {
+    public String divide(Double number1, Double number2) {
         if (number1 == null || number2 == null) {
             return "Вы ввели неверные данные!!!";
         } else if (number2 == 0) {
